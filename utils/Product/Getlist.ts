83c000a64072fbefd9417d9fab216a96e031d55f
amirhossein-k <amirhossein.k787878@gmail.com>
2023-09-18 14:06:32 +0330

    34

--- utils/Product/Getlist.ts
+++ utils/Product/Getlist.ts
@@ -1,10 +1,10 @@
 import prisma from "@/db/prismaDb";
-import {List} from "../../types";
+import {List, ListGet} from "../../types";
 
 export const GetList = async () => {
   try {
     console.log("r");
-    const list: List[] = await prisma.product.findMany({
+    const list: ListGet[] = await prisma.product.findMany({
       include: {
         list: true,
       },
